47465277b20cc42257e85ab045d0dc42f05e6e36
vandad1901 <vandad1901@gmail.com>
2026-04-22 01:08:59 -0400

    feat(api): added ginutil

---
+++ backend/utils/ginutil/types.go
@@ -0,0 +1,20 @@
+package ginutil
+
+type Response struct {
+	Success bool       `json:"success"`
+	Data    any        `json:"data,omitempty"`
+	Error   *ErrorInfo `json:"error,omitempty"`
+	Meta    *Meta      `json:"meta,omitempty"`
+}
+
+type ErrorInfo struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type Meta struct {
+	Page       int `json:"page,omitempty"`
+	PerPage    int `json:"per_page,omitempty"`
+	Total      int `json:"total,omitempty"`
+	TotalPages int `json:"total_pages,omitempty"`
+}
