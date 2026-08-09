d95a0a51b52bd21eada9285c047b012e9d933372
vandad1901 <vandad1901@gmail.com>
2026-08-09 19:48:00 -0400

    fix(auth): fix user table and definition

--- apps/auth/migrations/0001_create_users_table.up.sql
+++ apps/auth/migrations/0001_create_users_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users(
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     ---
-    username varchar(255) NOT NULL UNIQUE,
-    password_hash varchar(255) NOT NULL,
-    salt varchar(255) NOT NULL,
-    email varchar(255) NOT NULL UNIQUE,
+    username varchar(256) NOT NULL UNIQUE,
+    password_hash varchar(256) NOT NULL,
+    salt varchar(256) NOT NULL,
+    email varchar(256) NOT NULL UNIQUE,
     ---
     created_at timestamptz NOT NULL,
     updated_at timestamptz NOT NULL
