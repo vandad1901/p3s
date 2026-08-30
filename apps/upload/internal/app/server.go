97d76054163badaa40b6ec35bc5b752e42fc0c89
vandad1901 <vandad1901@gmail.com>
2026-08-29 21:49:14 -0400

    fix(upload): fix trailing slash problem

--- apps/upload/internal/app/server.go
+++ apps/upload/internal/app/server.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/vandad1901/p3s/apps/upload/internal/config"
 	uploadhttp "github.com/vandad1901/p3s/apps/upload/internal/upload/http"
 	"github.com/vandad1901/p3s/packages/go/apperror"
@@ -11,13 +12,16 @@ import (
 func initializeServers(a *App, cfg *config.Config) {
 	a.httpServer = echo.New()
 
+	a.httpServer.Pre(middleware.AddTrailingSlash())
+
 	g := a.httpServer.Group("/v1",
 		apperror.EchoMiddleware(a.logger),
-		authguard.EchoAuthGuard(a.logger, a.parser, a.keyfunc))
+		authguard.EchoAuthGuard(a.logger, a.parser, a.keyfunc),
+	)
 
 	registerHTTPHandlers(a, g)
 }
 
 func registerHTTPHandlers(a *App, g *echo.Group) {
-	uploadhttp.Register(g.Group("upload"), a.uploadService)
+	uploadhttp.Register(g.Group("/upload"), a.uploadService)
 }
