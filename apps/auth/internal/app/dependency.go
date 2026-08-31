8bdd107b6a5718371b6094adc895f6f426c47f12
vandad1901 <vandad1901@gmail.com>
2026-08-31 04:06:59 -0400

    refactor(auth): extract initializeDatabase to its own function for consistency

--- apps/auth/internal/app/dependency.go
+++ apps/auth/internal/app/dependency.go
@@ -7,9 +7,25 @@ import (
 	"github.com/vandad1901/p3s/apps/auth/internal/config"
 	"github.com/vandad1901/p3s/apps/auth/internal/token"
 	"github.com/vandad1901/p3s/packages/go/dbpattern"
+	"gorm.io/gorm"
 )
 
 func initializeDependencies(cfg *config.Config) (*App, error) {
+	db, err := initializeDatabase(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	signer := token.NewECDSASigner(cfg.JWTConfig.PrivateKey)
+
+	return &App{
+		db:     db,
+		signer: signer,
+		KeySet: jwkset.NewMemoryStorage(),
+	}, nil
+}
+
+func initializeDatabase(cfg *config.Config) (*gorm.DB, error) {
 	db := dbpattern.OpenDatabaseConnection(cfg.DSN)
 
 	sqlDB, err := db.DB()
@@ -22,11 +38,5 @@ func initializeDependencies(cfg *config.Config) (*App, error) {
 		return nil, fmt.Errorf("ping database: %w", err)
 	}
 
-	signer := token.NewECDSASigner(cfg.JWTConfig.PrivateKey)
-
-	return &App{
-		db:     db,
-		signer: signer,
-		KeySet: jwkset.NewMemoryStorage(),
-	}, nil
+	return db, nil
 }
