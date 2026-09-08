324957f444b6305b8768127c5b476089849d17b5
vandad1901 <vandad1901@gmail.com>
2026-09-07 21:47:20 -0400

    feat(packages): update dbpattern package

--- packages/go/dbpattern/postgres.go
+++ packages/go/dbpattern/postgres.go
@@ -6,17 +6,18 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const PostgresDSNFormat = "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC"
 
-func OpenDatabaseConnection(dsn string) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func OpenDatabaseConnection(dsn string, logger logger.Interface) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger})
 	if err != nil {
-		panic(fmt.Errorf("error connecting to database: %w", err))
+		return nil, fmt.Errorf("gorm opening: %w", err)
 	}
 
-	return db
+	return db, nil
 }
 
 func SerializableTx(db *gorm.DB, fn func(tx *gorm.DB) error) error {
