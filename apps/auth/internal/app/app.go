c5555dd223ec18169beb3e40134a1bcf47aeaa5d
vandad1901 <vandad1901@gmail.com>
2026-09-09 05:18:22 -0400

    feat(auth): removed root context

--- apps/auth/internal/app/app.go
+++ apps/auth/internal/app/app.go
@@ -72,10 +72,10 @@ func MustBoot(cfg *config.Config) *App {
 	return a
 }
 
-func (a *App) Serve(ctx context.Context, cfg *config.Config) error {
+func (a *App) Serve(cfg *config.Config) error {
 	servers := []func() error{
-		func() error { return serveGRPC(ctx, a, cfg) },
-		func() error { return serveHTTP(ctx, a, cfg) },
+		func() error { return serveGRPC(a, cfg) },
+		func() error { return serveHTTP(a, cfg) },
 	}
 
 	var runnerWG sync.WaitGroup
@@ -90,7 +90,7 @@ func (a *App) Serve(ctx context.Context, cfg *config.Config) error {
 
 	firstErr := <-errChan
 
-	a.Shutdown(ctx)
+	a.Shutdown()
 
 	runnerWG.Wait()
 	close(errChan)
@@ -108,10 +108,10 @@ func (a *App) Serve(ctx context.Context, cfg *config.Config) error {
 	return nil
 }
 
-func serveGRPC(ctx context.Context, a *App, cfg *config.Config) error {
+func serveGRPC(a *App, cfg *config.Config) error {
 	lc := net.ListenConfig{}
 
-	lis, err := lc.Listen(ctx, "tcp", cfg.GRPCListenAddress)
+	lis, err := lc.Listen(context.Background(), "tcp", cfg.GRPCListenAddress)
 	if err != nil {
 		return fmt.Errorf("grpc listen on %s: %w", cfg.GRPCListenAddress, err)
 	}
@@ -128,10 +128,10 @@ func serveGRPC(ctx context.Context, a *App, cfg *config.Config) error {
 	return nil
 }
 
-func serveHTTP(ctx context.Context, a *App, cfg *config.Config) error {
+func serveHTTP(a *App, cfg *config.Config) error {
 	lc := net.ListenConfig{}
 
-	lis, err := lc.Listen(ctx, "tcp", cfg.HTTPListenAddress)
+	lis, err := lc.Listen(context.Background(), "tcp", cfg.HTTPListenAddress)
 	if err != nil {
 		return fmt.Errorf("http listen on %s: %w", cfg.HTTPListenAddress, err)
 	}
@@ -154,9 +154,9 @@ func serveHTTP(ctx context.Context, a *App, cfg *config.Config) error {
 
 const shutdownTimeoutSecs = 10
 
-func (a *App) Shutdown(ctx context.Context) {
+func (a *App) Shutdown() {
 	a.shutdownOnce.Do(func() {
-		ctx, cancel := context.WithTimeout(context.WithoutCancel(ctx), shutdownTimeoutSecs*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeoutSecs*time.Second)
 		defer cancel()
 
 		var shutdownWG sync.WaitGroup
