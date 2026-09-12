bd4a2b56ed80e964e05e7380184bf7c1de6a90b8
vandad1901 <vandad1901@gmail.com>
2026-09-12 01:33:59 -0400

    fix(config): fix env variable

--- apps/media/internal/config/config.go
+++ apps/media/internal/config/config.go
@@ -17,35 +17,23 @@ type Config struct {
 	DSN string
 
 	AuthServiceAddress string
-
-	HTTPListenAddress string
 }
 
 func LoadConfig() *Config {
 	environment := envutil.MustGetEnvironment("APP_ENV")
 
-	var HTTPListenAddress string
-
-	switch environment {
-	case envutil.Development, envutil.Production:
-		HTTPListenAddress = envutil.MustGetString("HTTP_LISTEN_ADDRESS")
-	case envutil.Test:
-	}
-
 	return &Config{
 		Environment: environment,
 
-		RabbitMQAddress: envutil.MustGetString("UPLOAD_RMQ_ENDPOINT"),
+		RabbitMQAddress: envutil.MustGetString("MEDIA_RMQ_ENDPOINT"),
 
-		S3Endpoint:     envutil.MustGetString("UPLOAD_S3_ENDPOINT"),
-		S3RootUsername: envutil.MustGetString("UPLOAD_S3_ROOT_USERNAME"),
-		S3RootPassword: envutil.MustGetString("UPLOAD_S3_ROOT_PASSWORD"),
-		S3BucketName:   envutil.MustGetString("UPLOAD_S3_BUCKET_NAME"),
+		S3Endpoint:     envutil.MustGetString("MEDIA_S3_ENDPOINT"),
+		S3RootUsername: envutil.MustGetString("MEDIA_S3_ROOT_USERNAME"),
+		S3RootPassword: envutil.MustGetString("MEDIA_S3_ROOT_PASSWORD"),
+		S3BucketName:   envutil.MustGetString("MEDIA_S3_BUCKET_NAME"),
 
 		DSN: getDSN(),
 
 		AuthServiceAddress: envutil.MustGetString("AUTH_JWKS_ADDRESS"),
-
-		HTTPListenAddress: HTTPListenAddress,
 	}
 }
--- apps/media/internal/config/postgres.go
+++ apps/media/internal/config/postgres.go
@@ -8,11 +8,11 @@ import (
 )
 
 func getDSN() string {
-	host := gnrconfig.MustGetString("UPLOAD_PG_HOST")
-	port := gnrconfig.MustGetString("UPLOAD_PG_PORT")
-	user := gnrconfig.MustGetString("UPLOAD_PG_USER")
-	password := gnrconfig.MustGetString("UPLOAD_PG_PASSWORD")
-	dbName := gnrconfig.MustGetString("UPLOAD_PG_DATABASE")
+	host := gnrconfig.MustGetString("MEDIA_PG_HOST")
+	port := gnrconfig.MustGetString("MEDIA_PG_PORT")
+	user := gnrconfig.MustGetString("MEDIA_PG_USER")
+	password := gnrconfig.MustGetString("MEDIA_PG_PASSWORD")
+	dbName := gnrconfig.MustGetString("MEDIA_PG_DATABASE")
 
 	return fmt.Sprintf(dbpattern.PostgresDSNFormat, host, user, password, dbName, port)
 }
