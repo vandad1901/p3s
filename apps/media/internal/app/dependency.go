5dcfc7d1baf3ab8fa85a7b19e23e7e44bd764b3c
vandad1901 <vandad1901@gmail.com>
2026-09-22 15:20:42 -0400

    feat(media): remove jwks initialization in test

--- apps/media/internal/app/dependency.go
+++ apps/media/internal/app/dependency.go
@@ -12,6 +12,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/vandad1901/p3s/apps/media/internal/config"
 	"github.com/vandad1901/p3s/packages/go/dbpattern"
+	"github.com/vandad1901/p3s/packages/go/envutil"
 	"github.com/vandad1901/p3s/packages/go/gormslog"
 	"github.com/vandad1901/p3s/packages/go/rmqslog"
 	"github.com/wagslane/go-rabbitmq"
@@ -28,9 +29,11 @@ func initializeDependencies(a *App, cfg *config.Config) error {
 		return err
 	}
 
-	err = initializeJWT(a, cfg)
-	if err != nil {
-		return err
+	if cfg.Environment != envutil.Test {
+		err = initializeJWT(a, cfg)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = initializeRabbitMQ(a, cfg)
