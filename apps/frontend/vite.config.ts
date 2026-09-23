5e0b2840d16a764fdba2050aae247554806d69e5
vandad1901 <vandad1901@gmail.com>
2026-09-22 22:47:45 -0400

    fix(ui): fix tsconfigs for shadcn and vite

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
